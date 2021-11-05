80b09bed22f6cab80bfb16dc14ab5b8c857aabbd
Shudhant Gautam <shudh@RM0119-PF1KFC4M.americas.epicor.net>
2021-11-05 17:02:13 +0100

    Student List!

---
+++ StudentList/StudentList.cpp
@@ -0,0 +1,25 @@
+// This program is for the Student List assignment, which is to help teach about structs and pointers!
+// Author: Shudhant Gautam
+// Date: 07 November 2021
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+struct Student {
+  char fName;
+  char lName;
+  int ID;
+  float GPA;
+};
+
+void addFunc() {
+  
+}
+
+int main() {
+ 
+}
