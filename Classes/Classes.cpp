0eb953670931c8d26cd5474a7f2c3fecd511bf4c
Shudhant Gautam <369916@bsd48.org>
2021-12-03 16:45:37 +0000

    Update as of 12/2/2021

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -18,9 +18,15 @@ using namespace std;
 void ADD(vector<Media*>* media);
 void DELETE(char* title, vector<Media*>* media);
 void SEARCH(char* title, vector<Media*>* media);
+void SEARCH(int year, vector<Media*>* media);
+void DELETE(int year, vector<Media*>* media);
 
 int main() {
   bool running = true;
 
   char userInput = [20];
+  char titleIn = [50];
+  int yearIn;
+
+  vector<Media*> mediaList;
 }
