6eafa9fcd7400fb7a54e5555667a2a4e484b7a71
unknown <369916@bsd48.org>
2021-12-06 00:46:46 +0000

    Update as of 12/4, forgot to upload yesterday night

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -33,11 +33,56 @@ int main() {
   while (run) {
     cout << "Welcome! Would you like to SEARCH, ADD, DELETE, or QUIT?" << endl;
     cin.get(10);
+
+    // Clearing/ignoring buffers was a contribution of a prior C++ student, Mahmoud Abdelmoneum
     cin.clear();
     cin.ignore(10000, '\n');
 
-    if (strcmp(input, "title") == 0) {
-      cout << "Input title is: " << endl;
+    // If user command is "add" (or "ADD", we gotta keep track of capitalizations lol), we add!
+    if (strcmp(input, "add") == 0 || strcmp(input, "ADD") == 0) {
+      ADD(&mediaList);
+    }
+
+    // If user command is "search", we search!
+    else if (strcmp(input, "search") == 0 || strcmp(input, "SEARCH") == 0) {
+      // Ask user to specify search method
+      cout << "Do you want to search by title, or search by year?" << endl;
+
+      // Shoutout to Momo once again
+      cin.get(input, 6);
+      cin.clear();
+      cin.ignore(10000, '\n');
+
+      // If user specified "search by title"
+      if (strcmp(input, "title") == 0) {
+	cout << "Enter title: " << endl;
+
+	cin.get(title, 40);
+	cin.clear();
+	cin.ignore(10000, '\n');
+
+	// Now we search!
+	SEARCH(title, &mediaList);
+      }
+
+      // If user specified "search by year"
+      else if (strcmp(input, "year") == 0) {
+	cout << "Input year: " << endl;
+	cin >> year;
+
+	cin.clear();
+	cin.ignore(10000, '\n');
+
+	// Now we search, but this time, using the year
+	SEARCH(year, &mediaList);
+      }
+    }
+
+    // If user command is "delete", we delete!
+    else if (strcmp(input, "delete") == 0 || strcmp(input, "DELETE") == 0) {
+      cout << "Do you want to delete by title, or delete by year?" << endl;
+      // Don't need to get any more than 6 chars since "title" = 6 chars long (don't forget that last one lol this took me the longest time to debug)
+      cin.get(input, 6);
     }
   }
 }
