0156e86c99d966705825e5b0a5c3068c714e4302
unknown <369916@bsd48.org>
2021-12-20 04:58:10 +0000

    Update as of 12/19

--- Zuul/Item.h
+++ Zuul/Item.h
@@ -1,3 +1,25 @@
 // Header file for Items class in Zuul project
 // Author: Shudhant Gautam
 // Date: 19 December 2021
+
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Item {
+ public:
+  Item();
+  void setName(char*);
+  char* getName();
+  void setId(int);
+  int getId();
+ private:
+  char* name;
+  int id;
+};
+
+#endif
