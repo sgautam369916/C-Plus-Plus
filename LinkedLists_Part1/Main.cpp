b3c5fa27bb6d1d28787f1e25a5cd2d3ee5bcf9a8
Shudhant Gautam <369916@bsd48.org>
2022-01-10 03:37:53 +0000

    1st Status Update

--- LinkedLists_Part1/Main.cpp
+++ LinkedLists_Part1/Main.cpp
@@ -17,4 +17,58 @@ int main() {
   char userInput[30];
 
   // first student
+  Student* sdnt = new Student;
+
+  // first name
+  cout << "Please enter the student's first name: " << endl;
+  cin.get(sdnt->getFirst(), 30);
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // last name
+  cout << "Please enter the student's last name: " << endl;
+  cin.get(sdnt->getLast(), 30);
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // student ID#
+  cout << "Please enter the student's ID number: " << endl;
+  cin >> *sdnt->getID();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // GPA
+  cout << "Please enter the student's GPA: " << endl;
+  cin >> *sdnt->getGPA();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // second student
+  cout << " " << endl;
+  Student* sdnt2 = new Student;
+
+  // first name
+  cout << "Please enter the student's first name: " << endl;
+  cin.get(sdnt2->getFirst(), 30);
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // last name
+  cout << "Please enter the student's last name: " << endl;
+  cin.get(sdnt2->getLast(), 30);
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // student ID#
+  cout << "Please enter the student's ID number: " << endl;
+  cin >> *sdnt2->getID();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  // GPA
+  cout << "Please enter the student's GPA: " << endl;
+  cin >> *sdnt2->getGPA();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
 }
