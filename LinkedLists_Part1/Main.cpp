20c55d3ea99295389cc39fbc733fc43b3bdc67d5
Shudhant Gautam <369916@bsd48.org>
2022-01-10 03:15:58 +0000

    Initial commit because my school desktop wasn't working with GitHub for some reason

---
+++ LinkedLists_Part1/Main.cpp
@@ -0,0 +1,20 @@
+// Main program file for Linked Lists Part 1 project
+// Author: Shudhant Gautam
+// Date: 16 January 2022
+
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <stdio.h>
+
+#include "Node.h"
+#include "Student.h"
+
+// main
+int main() {
+
+  // userInput variable
+  char userInput[30];
+
+  // first student
+}
