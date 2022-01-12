9d42b52a7549a44f8209cfe3433060f01d53e003
Shudhant Gautam <369916@bsd48.org>
2022-01-12 01:22:55 +0000

    Lots of updates lol, I worked on my Mac but that doesn't have Git set up on it so I'm just bringing it onto my main PC and uploading it to Git that way...

---
+++ LinkedLists_Part1/Student.cpp
@@ -0,0 +1,40 @@
+// CPP file for Student class in Linked Lists Part 1 Project
+// Author: Shudhant Gautam
+// Date: 16 January 2022
+
+#include <iostream>
+#include <cstring>
+
+#include "Student.h"
+
+using namespace std;
+
+// (seemingly useless) constructor
+Student::Student() {
+
+}
+
+Student::~Student() {
+  delete &firstName;
+  delete &lastName;
+  delete &ID;
+  delete &GPA;
+}
+
+// getFirst
+char *Student::getFirst() {
+  return firstName;
+}
+
+// getLast
+char* Student::getLast() {
+  return lastName;
+}
+
+int* Student::getID() {
+  return &ID;
+}
+
+float* Student::getGPA() {
+  return &GPA;
+}
