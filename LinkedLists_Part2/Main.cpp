37bf912f361e895ff84d82614f1710ee7e1c75d0
Shudhant Gautam <369916@bsd48.org>
2022-01-23 19:42:38 +0000

    Update as of 01/22

--- LinkedLists_Part2/Main.cpp
+++ LinkedLists_Part2/Main.cpp
@@ -181,7 +181,113 @@ void removeStudent(Node* nextNode, Node* previousNode, char studentName[]) {
   char binaryUserInput;
   // If the next node is equal to the first node and the student's first names match up with the user's input
   if (nextNode == firstNode && strcmp(nextNode->getStudent()->getFirst(), studentName) == 0) {
-    
+    cout << nextNode->getStudent->getLast() << ", ";
+    cout << nextNode->getStudent()->getFirst() << endl;
+    cout << nextNode->getStudent()->getID() << endl;
+    cout << nextNode->getStudent()->getGPA() << endl;
+    // cout separator
+    cout << "===============" << endl;
+
+    cout << "Are you sure you want to delete this student from the list? (Y/N)" << endl;
+    cin >> binaryUserInput;
+    cin.clear();
+    cin.ignore(10000, '\n');
+
+    // If user said yes
+    if (binaryUserInput == 'Y' || binaryUserInput == 'y') {
+      cout << "The selected student has succesfully been deleted." << endl;
+
+      // Destructor to remove node
+      // If the next node is NULL, or if we are at the end of the list, then
+      if (nextNode->getNext() == NULL) {
+	// next node is destroyed
+	nextNode->~Node();
+	// first node is set to NULL
+	firstNode = NULL;
+      }
+      // Otherwise if we're not at the end of the list
+      else {
+	// set firstNode to the next node
+	firstNode = nextNode->getNext();
+	// Now we can safely destroy the nextNode
+	nextNode->~Node();
+	// calling remove() to remove the student from the list
+	remove(first, NULL, studentName);
+      }
+    }
+    // Otherwise if the user said no
+    else if (binaryUserInput == 'n' || binaryUserInput == 'N') {
+      cout << "Exited deleting student." << endl;
+      // If the next node is not NULL (meaning we're not at the end of the list yet)
+      if (nextNode->getNext() != NULL) {
+	// Then remove the next node and student
+	remove(nextNode->getNext(), nextNode, studentName);
+      }
+    }
+    // Invalid input
+    else {
+      cout << "ERROR: Invalid input. Please try again." << endl;
+    }
   }
-  
+  //
+  else if (strcmp(nextNode->getStudent()->getFirst(), studentName) == 0) {
+    cout << nextNode->getStudent()->getLast() << ", ";
+    cout << nextNode->getStudent()->getFirst() << endl;
+    cout << nextNode->getStudent()->getID() << endl;
+    cout << nextNode->getStudent()->getGPA() << endl;
+    cout << "===============" << endl;
+
+    cout << "Are you sure you want to delete this student from the list? (Y/N)" << endl;
+    cin >> binaryUserInput;
+    cin.clear();
+    cin.ignore(10000, '\n');
+
+    // If user said yes
+    if (binaryUserInput == 'y' || binaryUserInput == 'Y') {
+      cout << "The selected tudent has succesfully been deleted." << endl;
+      // Doubly-linked lists: going backwards in the list
+      previousNode->setNext(nextNode->getNext());
+      // Now we can safely destroy the next node
+      nextNode->~Node();
+    }
+    // Otherwise if user said no
+    if (binaryUserInput == 'n' || binaryUserInput == 'N') {
+      cout << "Exited deleting student." << endl;
+    }
+    // Invalud input
+    else {
+      cout << "ERROR: Invalid input. Please try again." << endl;
+    }
+  }
+  // If the next node is not NULL (meaning we're not at the end of the list yet)
+  if (nextNode->getNext() != NULL) {
+    // Then remove the next node and the student
+    remove(nextNode->getNext(), nextNode, studentName);
+  }
+}
+
+// Average
+float average(Node* nextNode) {
+  float totalVal = 0;
+  // Number of GPA's (AKA number of students)
+  int count = 0;
+
+  // If the current node is the first node (iterating through the list)
+  if (nxtNode == firstNode) {
+    // Then add current node's student's GPA value to totalVal
+    totalVal += nextNode->getStudent()->getGPA();
+    // Increase count
+    count++;
+  }
+  // As long as the next node is not NULL (meaning we haven't reached the end of hte list yet)
+  while (nextNode->getNext() != NULL) {
+    // Add the next student's GPA to totalVal
+    totalVal += nextNode->getNext()->getStudent->getGPA();
+    // Increase count
+    count++;
+    // Move on to the next node (iteration)
+    nextNode = nextNode->getNext();
+  }
+  // Return the average
+  return (totalVal/count);
 }
